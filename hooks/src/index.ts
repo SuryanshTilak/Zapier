8a64c1fa74a82692b10ee81299432aa7545600cd
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-09-05 19:58:09 +0530

    1- zapier schema

---
+++ hooks/src/index.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const app=express()
+const client=new PrismaClient()
+
+app.use(express.json())
+
+app.listen(5000 ,() => {
+    console.log("running")
+})
+
+
+app.get('/', function (req, res) {  
+  res.send('Welcome to my world who are u mister');  
+}); 
+
+app.get('/hooks/catch',(req,res) =>{
+    res.send('hi')
+})
+
+app.post('/hooks/catch/:userId/:zapId',async(req,res)=>{
+    const userId=req.params.userId
+    const zapId=req.params.zapId
+    const body=req.body
+    
+    client.$transaction(async tx =>{
+        const run=await client.zapRun.create({
+            data : {
+                zapId : zapId,
+                metadata : body
+            }
+        })
+
+        await client.zapRunOutBox.create({
+            data : {
+                zapRunId : run.id
+            }
+        })
+    })
+
+    console.log(userId+","+zapId)
+    res.send("Successful")
+})
+
