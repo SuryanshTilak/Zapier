bcba164cf6874a610232dca7544051ffeb4ebde7
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-09-10 00:56:29 +0530

    user routes updated

--- primary_backend/src/router/user.ts
+++ primary_backend/src/router/user.ts
@@ -1,21 +1,103 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware";
-import { SignupSchema } from "../types";
+import { SigninSchema, SignupSchema } from "../types";
+import { PrismaClient } from "@prisma/client";
+import { prismaClient } from "../db";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../types/config";
 
-const  router=Router()
+const router = Router();
+const client = new PrismaClient();
 
-router.post('/signup',(req,res)=>{
-    const body=req.body.userName
-    const parseData = SignupSchema.safeParse(body)
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const parseData = SignupSchema.safeParse(body);
+
+  if (!parseData.success) {
+    return res.status(411).json({
+      message: "Incorrect Inputs",
+    });
+  }
+
+  const userExists = await prismaClient.user.findFirst({
+    where: {
+      email: parseData.data.email
+    }
+  });
+
+  if(userExists)
+  {
+    return res.status(403).json({
+        message : "User already Exist"
+    })
+  }
+
+  await prismaClient.user.create({
+    data : {
+        email : parseData.data.email,
+        password : parseData.data.password,
+        name     : parseData.data.name
+    }
+  })
+
+  return res.json({
+    message : "Please verify your account"
+  })
+
+});
+
+
+
+router.post("/signin",async (req, res) => {
+    const body=req.body
+    const parseData = SigninSchema.safeParse(body)
+
+    if(!parseData.success)
+    return res.status(403).json({
+        message : "Wrong email or password"
+    })
+
+    const userExists = await prismaClient.user.findFirst({
+        where : {
+            email    : parseData.data?.email,
+            password : parseData.data?.password
+        }
+    })
+
+    if(!userExists)
+    return res.json({
+        message : "Sorry credentials are incorrect"
 })
 
-router.post('/signin',(req,res) =>{
-    console.log("signin handler")
-})
+    const token=jwt.sign({
+        id : userExists.id
+    },JWT_PASSWORD)
+    
+    res.json({
+        token : token
+    })
+ 
 
-router.get("/user",authMiddleware,(req,res) =>{
-    res.send("hi")
-    console.log("signup handler")
-})
+});
 
-export const userRouter=router
+router.get("/user", authMiddleware, async(req, res) => {
+    
+    // @ts-ignore
+    const id=req.id
+    const user =await prismaClient.user.findFirst({
+        where : {
+            id : id
+        },
+        select : {
+            name : true,
+            email : true
+
+        }
+    })
+
+    return res.json({
+        user
+    })
+});
+
+export const userRouter = router;
--- primary_backend/src/types/index.ts
+++ primary_backend/src/types/index.ts
@@ -1,11 +1,12 @@
 import {z} from "zod"
 
 export const SignupSchema = z.object({
-    username : z.string(),
-    password : z.string()
+    email : z.string().min(5),
+    password : z.string().min(6),
+    name     : z.string().min(3)
 })
 
 export const SigninSchema = z.object({
-    userName : z.string(),
+    email : z.string(),
     password : z.string()
 })
